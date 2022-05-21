f24a2c9234c878d27d4dcdb5ded0d74b2d5c3423
eludadev <111eluda111@gmail.com>
2022-05-21 20:16:12 +0100

    Remove contributions site

--- contributions-site/api/node_modules/express-async-handler/index.js
+++
@@ -1,8 +0,0 @@
-const asyncUtil = fn =>
-function asyncUtilWrap(...args) {
-  const fnReturn = fn(...args)
-  const next = args[args.length-1]
-  return Promise.resolve(fnReturn).catch(next)
-}
-
-module.exports = asyncUtil
