26e816db8bcdd5cd96ba972f4d98b1bf125e6488
eludadev <111eluda111@gmail.com>
2022-05-28 16:08:24 +0100

    Add website

---
+++ website/src/components/ButtonItem.vue
@@ -0,0 +1,36 @@
+<script>
+	export default {
+		props: {
+			id: {
+				type: Number,
+				required: true
+			}
+		},
+		data() {
+			return {
+				loading: true
+			}
+		},
+		mounted() {
+			this.loading = false
+		}
+	}
+</script>
+
+<template>
+	<div v-if="loading" class="w-full h-48 rounded-xl
+    bg-stone-50/10 backdrop-blur-lg animate-pulse">
+    </div>
+	<a v-else href="#">
+		<article class="flex flex-col
+	w-full h-48 border-2 border-stone-50 rounded-xl
+	bg-stone-50/10 backdrop-blur-lg">
+			<header class="order-2 h-8 grow-0">
+				<h1 class="ml-2 font-bold text-lg text-stone-50">Basic</h1>
+			</header>
+			<main class="grow grid place-items-center">
+				<button>Button {{ id }}</button>
+			</main>
+		</article>
+	</a>
+</template>
---
+++ website/src/views/HomeView.vue
@@ -0,0 +1,46 @@
+<script>
+    import ButtonItem from '@/components/ButtonItem.vue'
+
+    import buttonsBannerUrl from '@/assets/buttons-banner.png'
+
+
+    export default {
+        components: {
+            ButtonItem
+        },
+        data() {
+            return {
+                buttonsBannerUrl,
+                buttons: []
+            }
+        },
+        async mounted() {
+            // Get total buttons count
+            const total = await this.getTotalCount()
+            console.log(total)
+            // 
+        },
+        methods: {
+            async loadButton(id) {
+                this.buttons.push({id: Math.max(...this.buttons.map(b => b.id)) + 1})
+            }
+        }
+    }
+</script>
+
+<template>
+    <header class="relative w-full h-56">
+        <img class="w-3/4 sm:w-1/2 lg:w-96 absolute
+        top-1/2 transform -translate-y-1/2
+        left-1/2 transform -translate-x-1/2
+        " :src="buttonsBannerUrl"/>
+    </header>
+
+    <main class="mx-2 mt-8">
+        <ul class="flex flex-col gap-4" ref="buttonsList">
+            <li v-for="{ id } in buttons">
+                <button-item :id="Number(id)"/>
+            </li>
+        </ul>
+    </main>
+</template>
